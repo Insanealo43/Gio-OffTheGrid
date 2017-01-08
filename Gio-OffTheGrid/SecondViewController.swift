4eb5dc13a0aec57768a819e44cd750a73e48c49e
Andrew Lopez-Vass <alopezvass@isbx.com>
2017-01-07 19:17:51 -0800

    Initial Commit

---
+++ Gio-OffTheGrid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gio-OffTheGrid
+//
+//  Created by Andrew Lopez-Vass on 1/7/17.
+//  Copyright © 2017 Andrew Lopez-Vass. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
