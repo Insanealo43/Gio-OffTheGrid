e4b79efe57871075265ed6078aa1821375f1b502
Andrew Lopez-Vass <alopezvass@isbx.com>
2017-01-08 14:16:45 -0800

    Refactored OTGManager enums.

--- Gio-OffTheGrid/OTGManager.swift
+++ Gio-OffTheGrid/OTGManager.swift
@@ -8,49 +8,62 @@
 
 import Foundation
 
-public enum OffTheGridUrls: String {
-    case OTGVendors = "https://www.offthegridmarkets.com/api/v1.0/vendors.json"
-    case FBGraphOTGEvents = "https://graph.facebook.com/v2.8/OffTheGridSF/events"
+public enum OffTheGrid {
+    enum Urls: String {
+        case OTGVendors = "https://www.offthegridmarkets.com/api/v1.0/vendors.json"
+        case FBGraphOTGEvents = "https://graph.facebook.com/v2.8/OffTheGridSF/events"
+    }
+    
+    enum Facebook {
+        static let AppId = "211025409359530"
+        static let AppSecret = "87a273cbaaf2364e16652a4d6a0b2111"
+        static let AccessToken = "\(Facebook.AppId)|\(Facebook.AppSecret)"
+    }
 }
 
 class OTGManager {
     static let sharedInstance = OTGManager()
     
-    internal enum Constants {
-        static let FBAppId = "211025409359530"
-        static let FBAppSecret = "87a273cbaaf2364e16652a4d6a0b2111"
-        static let FBAccessToken = "\(Constants.FBAppId)|\(Constants.FBAppSecret)"
+    internal enum QueryParams {
+        enum Keys {
+            static let accessToken = "access_token"
+            static let sortOrder = "sort-order"
+        }
+        
+        enum Values {
+            static let nameAscending = "name-asc"
+        }
     }
     
-    internal enum Keys {
-        static let accessToken = "access_token"
-        static let sortOrder = "sort-order"
-        static let nameAscending = "name-asc"
-    }
-    
-    internal enum JSON {
+    internal enum JSONKeys {
         static let data = "data"
         static let vendors = "Vendors"
     }
     
+    // MARK - Events
     func fetchEvents() {
-        let eventsUrl = OffTheGridUrls.FBGraphOTGEvents.rawValue
-        let params = [Keys.accessToken: Constants.FBAccessToken as AnyObject]
+        let eventsUrl = OffTheGrid.Urls.FBGraphOTGEvents.rawValue
+        let params = [QueryParams.Keys.accessToken: OffTheGrid.Facebook.AccessToken as AnyObject]
         
         NetworkManager.sharedInstance.request(url: eventsUrl, parameters: params, handler: { response in
-            if let eventsJSON = response?[JSON.data] as? [AnyObject] {
-                print("OTG Events JSON --> \(eventsJSON)")
+            if let eventsJSON = response?[JSONKeys.data] as? [AnyObject] {
+                print("OTG Events(\(eventsJSON.count)) JSON --> \(eventsJSON)")
             }
         })
     }
     
+    func fetchUpcomingEvents() {
+        
+    }
+    
+    // MARK - Vendors
     func fetchVendors() {
-        let vendorsUrl = OffTheGridUrls.OTGVendors.rawValue
-        let params = [Keys.sortOrder: Keys.nameAscending as AnyObject]
+        let vendorsUrl = OffTheGrid.Urls.OTGVendors.rawValue
+        let params = [QueryParams.Keys.sortOrder: QueryParams.Values.nameAscending as AnyObject]
         
         NetworkManager.sharedInstance.request(url: vendorsUrl, parameters: params, handler: { response in
-            if let vendorsJSON = response?[JSON.vendors] as? [AnyObject] {
-                print("OTG Vendors JSON --> \(vendorsJSON)")
+            if let vendorsJSON = response?[JSONKeys.vendors] as? [AnyObject] {
+                print("OTG Vendors(\(vendorsJSON.count)) JSON --> \(vendorsJSON)")
             }
         })
     }
