2491c1e80d649fbd5f5164a85c70c6a0e465bc16
Andrew Lopez-Vass <alopezvass@isbx.com>
2017-01-07 19:22:49 -0800

    Integrated Cocoapods.

---
+++ Gio-OffTheGrid/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Gio-OffTheGrid
+//
+//  Created by Andrew Lopez-Vass on 1/7/17.
+//  Copyright © 2017 Andrew Lopez-Vass. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager {
+    
+}
