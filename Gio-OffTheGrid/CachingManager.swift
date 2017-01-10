a19928a06557afaad8d891cea8c0ab0e28ec411c
Andrew Lopez-Vass <alopezvass@isbx.com>
2017-01-10 07:24:56 -0800

    Created CachingManager class to refactor PersistanceManager.

---
+++ Gio-OffTheGrid/CachingManager.swift
@@ -0,0 +1,15 @@
+//
+//  CachingManager.swift
+//  Gio-OffTheGrid
+//
+//  Created by Andrew Lopez-Vass on 1/10/17.
+//  Copyright © 2017 Andrew Lopez-Vass. All rights reserved.
+//
+
+import UIKit
+import DataCache
+
+class CachingManager {
+    static let sharedInstance = CachingManager()
+    
+}
