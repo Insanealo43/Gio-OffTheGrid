be061d2cde0812c2fec94b374992e59047ca9ba6
Andrew Lopez-Vass <alopezvass@isbx.com>
2017-01-10 07:27:12 -0800

    Moved over needed logic for CachingManager class.

--- Gio-OffTheGrid/CachingManager.swift
+++ Gio-OffTheGrid/CachingManager.swift
@@ -12,4 +12,39 @@ import DataCache
 class CachingManager {
     static let sharedInstance = CachingManager()
     
+    internal enum Constants {
+        static let maxDayLimit = 30
+    }
+    
+    // MARK - Cache Loading
+    func loadJSON(from cacheKey:String) -> JSONObject? {
+        if let data = DataCache.instance.readData(forKey: cacheKey) {
+            return self.convertToJSON(from: data)
+        }
+        return nil
+    }
+    
+    // MARK - Cache Saving
+    func saveJSON(json:JSONObject, with cacheKey: String) {
+        if let data = self.convertToData(from: json) {
+            DataCache.instance.write(data: data, forKey: cacheKey)
+        }
+    }
+    
+    // MARK - (JSONObject <-> Data) Conversions
+    internal func convertToData(from json: JSONObject) -> Data? {
+        do {
+            return try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+        } catch _ {
+            return nil
+        }
+    }
+    
+    internal func convertToJSON(from data: Data) -> JSONObject? {
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? JSONObject
+        } catch _ {
+            return nil
+        }
+    }
 }
